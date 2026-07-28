fc5161edc0428188418f6a4ed3ce3758b4213214
mhmkhavari3-collab <mhmkhavari3@gmail.com>
2026-07-29 01:32:27 +0330

    Minimal Application class

--- app/src/main/java/com/tradevision/TradeVisionApplication.kt
+++ app/src/main/java/com/tradevision/TradeVisionApplication.kt
@@ -1,12 +1,13 @@
 package com.tradevision
 
 import android.app.Application
-import android.util.Log
-import com.tradevision.auth.AuthManager
 
 class TradeVisionApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        try { AuthManager.initialize(this) } catch (e: Exception) { Log.e("App", "Auth init failed", e) }
+        try {
+            com.tradevision.auth.AuthManager.initialize(this)
+        } catch (_: Exception) {
+        }
     }
 }
