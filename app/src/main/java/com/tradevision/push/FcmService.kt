f32eb38e54aaf828952a382a7c4d914147e6a1fa
mhmkhavari3-collab <mhmkhavari3@gmail.com>
2026-07-29 01:13:02 +0330

    Minimal FcmService

--- app/src/main/java/com/tradevision/push/FcmService.kt
+++ app/src/main/java/com/tradevision/push/FcmService.kt
@@ -1,12 +1,2 @@
 package com.tradevision.push
-
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.util.Log
-
-class FcmService : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        Log.d("FcmService", "Received: ${intent.action}")
-    }
-}
+class FcmService
